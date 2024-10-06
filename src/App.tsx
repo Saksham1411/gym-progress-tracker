d7f4ad4a12824850a18954fafa3f25a1a1420599
Saksham1411 <sakshamsamgarg1411@gmail.com>
2024-10-06 18:43:15 +0530

    vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Accordian from "./components/Accordian/Accordian"
 import Header from "./components/Header/Header"
-
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
     <div>
+      <Analytics/>
       <Header/>
       <Accordian/>
     </div>
