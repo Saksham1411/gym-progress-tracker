e84dd073cdcb2e07c52f04a90132e6f9e9fed406
Saksham1411 <sakshamsamgarg1411@gmail.com>
2024-10-11 17:53:40 +0530

    setup firbase and signup and login is completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import Accordian from "./components/Accordian/Accordian"
-import Header from "./components/Header/Header"
-import { Analytics } from "@vercel/analytics/react"
+import { Route, Routes } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
+import { Toaster } from "react-hot-toast"
+import HomePage from "./pages/HomePage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
 
 const App = () => {
   return (
-    <div>
-      <Analytics/>
-      <Header/>
-      <Accordian/>
-    </div>
-  )
-}
+    <>
+      <Analytics />
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
