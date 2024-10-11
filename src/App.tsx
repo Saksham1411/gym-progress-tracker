7ebfd4acadb11b1e0a2059f6fce9067272ef3f6e
Saksham1411 <sakshamsamgarg1411@gmail.com>
2024-10-11 18:49:15 +0530

    removed get data button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Accordian from "./components/Accordian/Accordian"
 import Header from "./components/Header/Header"
-import SaveDataButton from "./components/SaveDataButton"
 import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
@@ -9,7 +8,6 @@ const App = () => {
       <Analytics/>
       <Header/>
       <Accordian/>
-      <SaveDataButton/>
     </div>
   )
 }
